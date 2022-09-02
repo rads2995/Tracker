a043700c5e11326479a15e75cf543c6f2ef62740
Rodrigo Duran <Rads2995@gmail.com>
2022-09-02 01:32:04 -0500

    Updates to automated Jenkins in docker

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Create a bridge network in Docker
+docker network create jenkins
+
+# To execute Docker commands inside Jenkins nodes, download/run docker:dind
+docker run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind --storage-driver overlay2
+
+# Build a new docker image from the Dockerfile
 docker build -t jenkins-test .
 
+# Run image as a container in Docker
 docker run --name jenkins-test --restart=on-failure --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
