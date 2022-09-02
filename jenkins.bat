d3a6f11cfe1ca7ea021f86f86e069fe7eea0ddee
Rodrigo Duran <Rads2995@gmail.com>
2022-09-02 16:52:44 -0500

    Initial Batch script for docker setup on Windows

---
+++ jenkins.bat
@@ -0,0 +1,24 @@
+# Create a bridge network in Docker
+docker network create jenkins
+
+# To execute Docker commands inside Jenkins nodes, download/run docker:dind
+docker run --name jenkins-docker --rm --detach ^
+  --privileged --network jenkins --network-alias docker ^
+  --env DOCKER_TLS_CERTDIR=/certs ^
+  --volume jenkins-docker-certs:/certs/client ^
+  --volume jenkins-data:/var/jenkins_home ^
+  --publish 2376:2376 ^
+  docker:dind
+
+# Build a new docker image from the Dockerfile
+docker build -t jenkins-test .
+
+# Run image as a container in Docker
+docker run --name jenkins-test --restart=on-failure --detach ^
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 ^
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 ^
+  --volume jenkins-data:/var/jenkins_home ^
+  --volume jenkins-docker-certs:/certs/client:ro ^
+  --publish 8080:8080 --publish 50000:50000 ^
+  jenkins-test
+  
